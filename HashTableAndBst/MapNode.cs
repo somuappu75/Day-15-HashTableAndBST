aafbf99b7596064857d9fe6efdc7cd31f6277d11
Lenovo <somusp75@gmail.com>
2022-01-22 15:22:46 +0530

    Uc-1 Finding Frequency Of words

--- HashTableAndBst/MapNode.cs
+++ HashTableAndBst/MapNode.cs
@@ -4,5 +4,95 @@ using System.Text;
 
 namespace HashTableAndBst
 {
-    
+    // KeyValue for data type And Value
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    };
+
+    public class MapNode<K, V>
+    {
+        int size;
+        public LinkedList<KeyValue<K, V>>[] items;
+        public MapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListofPosition = GetLinkedListPosition(position);
+            KeyValue<K, V> keyValue = new KeyValue<K, V>()
+            {
+                Key = key,
+                Value = value
+            };
+            LinkedListofPosition.AddLast(keyValue);
+        }
+
+        // Get array position
+        public int GetArrayPosition(K key)
+        {
+            int hashcode = key.GetHashCode();
+            int position = hashcode % size;
+            return Math.Abs(position);
+        }
+
+        //Create linkedlist for a particular position
+        public LinkedList<KeyValue<K, V>> GetLinkedListPosition(int position)
+        {
+            if (items[position] == null)
+            {
+                items[position] = new LinkedList<KeyValue<K, V>>();
+            }
+            return items[position];
+        }
+        // Check if element is already Present
+        public int CheckHash(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListofPosition = GetLinkedListPosition(position);
+            int count = 1;
+            bool found = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+
+            foreach (KeyValue<K, V> keyValue in LinkedListofPosition)
+            {
+                if (keyValue.Key.Equals(key))
+                {
+                    count = Convert.ToInt32(keyValue.Value) + 1;
+                    found = true;
+                    founditem = keyValue;
+                }
+            }
+            if (found)
+            {
+                LinkedListofPosition.Remove(founditem);
+                return count;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+        // for particular key Display Linkedlist elements
+        public void Display(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListofPosition = GetLinkedListPosition(position);
+            foreach (KeyValue<K, V> keyValue in LinkedListofPosition)
+            {
+                if (keyValue.Key.Equals(key))
+                {
+                    Console.WriteLine("Key: " + keyValue.Key + "\t Value: " + keyValue.Value);
+                }
+
+            }
+        }
+    }
 }
--- HashTableAndBst/Program.cs
+++ HashTableAndBst/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HashTableAndBst
 {
@@ -6,7 +8,49 @@ namespace HashTableAndBst
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("-----------------  ## Frequency Of Words Using Hash function ## --------------");
+            Console.WriteLine("------------UC-1- find frequency of words -------- ");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+                Console.WriteLine("Enter Your Option\n1.FRequency Of Words");
+                int num = Convert.ToInt32(Console.ReadLine());
+                MapNode<string, int> myMapNode = new MapNode<string, int>(6);
+                switch (num)
+                {
+                    case 1:
+                        string[] words = { "to", "be", "or", "not", "to", "be" };
+                        int count = 1;
+                        foreach (string i in words)
+                        {
+                            count = myMapNode.CheckHash(i);
+                            if (count > 1)
+                            {
+                                myMapNode.Add(i, count);
+                            }
+                            else
+                            {
+                                myMapNode.Add(i, 1);
+                            }
+                        }
+
+                        IEnumerable<string> uniqueItems = words.Distinct<string>();
+                        foreach (var i in uniqueItems)
+                        {
+                            myMapNode.Display(i);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!!!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue?(Y/N)");
+                flag = Console.ReadLine();
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
